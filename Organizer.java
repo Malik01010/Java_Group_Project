7a8f427e13017bf8296d4b8eb2d6a38f8a9ac202
RONJ0N <101667694+RONJ0N@users.noreply.github.com>
2024-04-22 19:42:29 -0400

    Update Organizer.java

--- Organizer.java
+++ Organizer.java
@@ -1,45 +1,58 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Scanner;
 
 public class Organizer {
-    private String organizerName;
     private ArrayList<Event> events;
     private HashMap<String, HashSet<String>> attendeeRSVPs;
-    private HashMap<String, HashMap<String, String>> eventFeedback;
+    private HashMap<String, ArrayList<Customer>> eventCustomers;
+    private ArrayList<Customer> allCustomers;
 
     public Organizer(String name) {
-        this.organizerName = name;
         this.events = new ArrayList<>();
         this.attendeeRSVPs = new HashMap<>();
-        this.eventFeedback = new HashMap<>();
+        this.eventCustomers = new HashMap<>();
+        this.allCustomers = new ArrayList<>();
     }
 
-    public void createEvent(String name, String date, String type, int max, String[] speakers) {
-        if (type.equals("Conference")) {
-            events.add(new Conference(name, date, max, speakers));
-        } else {
-            // Handle other types of events
-        }
+    public void createEvent(String name, String date, ArrayList<String> speakers) {
+        events.add(new Event(name, date));
+        
     }
+    
+   
 
-    public void RSVP(String attendeeName, String eventName) {
-        if (!attendeeRSVPs.containsKey(attendeeName)) {
-            attendeeRSVPs.put(attendeeName, new HashSet<>());
+    public void RSVP(Customer customer, String eventName) {
+        if (!eventCustomers.containsKey(eventName)) {
+            eventCustomers.put(eventName, new ArrayList<>());
         }
-        attendeeRSVPs.get(attendeeName).add(eventName);
-    }
-
-    public void submitFeedback(String eventName, String attendeeName, String feedback) {
-        if (!eventFeedback.containsKey(eventName)) {
-            eventFeedback.put(eventName, new HashMap<>());
-        }
-        eventFeedback.get(eventName).put(attendeeName, feedback);
+        eventCustomers.get(eventName).add(customer);
     }
 
     public ArrayList<Event> getEvents() {
         return events;
     }
+    
+    public void addCustomer(Customer customer) {
+        allCustomers.add(customer);
+    }
 
-    // Implement additional methods as needed
+    public ArrayList<Customer> getAllCustomers() {
+        return allCustomers;
+    }
+    public Customer findCustomerByName(String name) {
+        for (Customer customer : allCustomers) {
+            if (customer.getName().equals(name)) {
+                return customer;
+            }
+        }
+        return null; // Customer not found
+    }
+
+    public boolean customerAttendedEvent(Customer customer, String eventName) {
+        ArrayList<Customer> customers = eventCustomers.get(eventName);
+        return customers != null && customers.contains(customer);
+    }
+
 }
