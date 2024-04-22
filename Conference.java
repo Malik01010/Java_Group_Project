11fa2c9a241d1de5289eb1c61f63c4a4510dbdfe
RONJ0N <101667694+RONJ0N@users.noreply.github.com>
2024-04-22 14:57:17 -0400

    Update Conference.java

--- Conference.java
+++ Conference.java
@@ -1,18 +1,1 @@
-public class Conference extends Event {
-    private int maxAttendees;
-    private String[] guestSpeakers;
 
-    public Conference(String name, String date, int max, String[] speakers) {
-        super(name, date, "Conference");
-        this.maxAttendees = max;
-        this.guestSpeakers = speakers;
-    }
-
-    public int getMaxAttendees() {
-        return maxAttendees;
-    }
-
-    public String[] getGuestSpeakers() {
-        return guestSpeakers;
-    }
-}
