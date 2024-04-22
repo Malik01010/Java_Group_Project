95ad69647c01b1079242b82c04151367aec645f6
Brooks-Ruehle <166452360+Brooks-Ruehle@users.noreply.github.com>
2024-04-22 16:15:12 -0400

    Update Organizer.java

--- Organizer.java
+++ Organizer.java
@@ -3,21 +3,18 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class Organizer {
-   
     private ArrayList<Event> events;
     private HashMap<String, HashSet<String>> attendeeRSVPs;
-  
+    private HashMap<String, ArrayList<Customer>> eventCustomers;
 
     public Organizer(String name) {
- 
         this.events = new ArrayList<>();
         this.attendeeRSVPs = new HashMap<>();
+        this.eventCustomers = new HashMap<>();
     }
 
-    public void createEvent(String name, String date, String[] speakers) {
-      
-            events.add(new Event(name, date));
-        
+    public void createEvent(String name, String date, ArrayList<String> speakers) {
+        events.add(new Event(name, date));
     }
 
     public void RSVP(String attendeeName, String eventName) {
@@ -27,9 +24,19 @@ public class Organizer {
         attendeeRSVPs.get(attendeeName).add(eventName);
     }
 
+    public void RSVP(Customer customer, String eventName) {
+        if (!eventCustomers.containsKey(eventName)) {
+            eventCustomers.put(eventName, new ArrayList<>());
+        }
+        eventCustomers.get(eventName).add(customer);
+    }
 
     public ArrayList<Event> getEvents() {
         return events;
+    }
+
+    public ArrayList<Customer> getCustomersForEvent(String eventName) {
+        return eventCustomers.get(eventName);
     }
 
     // Implement additional methods as needed
